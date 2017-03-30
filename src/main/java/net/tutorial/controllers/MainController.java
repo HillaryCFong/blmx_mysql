34100f948a15c04edcc4ec80391c14addb5b5272
Hillary Fong <rfong36@yahoo.com>
2017-03-30 10:45:00 +0000

    test1

--- src/main/java/net/tutorial/controllers/MainController.java
+++ src/main/java/net/tutorial/controllers/MainController.java
@@ -39,7 +39,7 @@ public class MainController extends HttpServlet {
 				db.deleteRecord(Integer.parseInt(id));
 			}
 <<<<<<< Upstream, based on 144400927d5463a95869e39cd08857f817d08298
-			req.setAttribute("contacts", db.allRecords());
+			req.setAttribute("translations", db.allRecords());
 =======
 
 			req.setAttribute("translations", db.allRecords());
