bf952d402901402637411391c5807d50129f288d
Hillary Fong <rfong36@yahoo.com>
2017-03-30 11:25:29 +0000

    test

--- src/main/java/net/tutorial/controllers/MainController.java
+++ src/main/java/net/tutorial/controllers/MainController.java
@@ -58,8 +58,8 @@ public class MainController extends HttpServlet {
 		String language = req.getParameter("language");
 		String old = req.getParameter("old");
 		String newt = req.getParameter("newt");
-		String thumbsup = req.getParameter("thumbsup");
-		String thumbsdown = req.getParameter("thumbsdown");
+		Int thumbsup = req.getParameter("thumbsup");
+		Int thumbsdown = req.getParameter("thumbsdown");
 
 		Map<String, Object> record = new HashMap<String, Object>();
 		DBService db = DBService.getInstance();
