0fbd74f71b70115917701b52ebe1f2837c7a8a50
paulkaTea <tee.paulina@gmail.com>
2017-04-01 20:33:49 +0800

    oh

--- src/main/java/net/tutorial/controllers/TranslateController.java
+++ src/main/java/net/tutorial/controllers/TranslateController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import net.tutorial.utilities.DBService;
 
 @WebServlet("/translate")
-public class MainController extends HttpServlet {
+public class TranslateController extends HttpServlet {
 	RequestDispatcher dispatcher;
 	DBService db = null;
 
