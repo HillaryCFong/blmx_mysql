d5ac9f49c1cb87fec1087c6ffa8b88a9915a6ef9
royceocampo <royceocampo@yahoo.com>
2017-04-04 11:08:42 +0800

    Revert "hngggg"
    This reverts commit 2a3b60e8783db82a2599751eeb32b27aa9d06b0a.

--- src/main/java/net/tutorial/controllers/MainController.java
+++ src/main/java/net/tutorial/controllers/MainController.java
@@ -127,10 +127,10 @@ public class MainController extends HttpServlet {
 					c.setMaxAge(60*15);
 					resp.addCookie(c);
 						
-					resp.sendRedirect("trans?action=trans");
+					resp.sendRedirect("/trans");
 					}
 					else 
-					resp.sendRedirect("login?action=login");
+					resp.sendRedirect("/login");
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
